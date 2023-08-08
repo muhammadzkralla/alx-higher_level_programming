b9bcb5ceb34f07261c0149beddb89f58468a3076
Mohamed Zkrallah <54005330+muhammadzkralla@users.noreply.github.com>
2023-08-08 13:39:31 +0300

    Type Error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = int(str(number)[len(str(number)) - 1])
+digit = abs(number) % 10
 if number < 0:
     digit = -digit
 print(f"Last digit of {number:d} is {digit:d} and is ", end="")
