a12d012719a5bc7f88c8e3a28e6b74fa0b62ed65
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-08-08 13:38:33 +0200

    Add and Pow

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Function add two numbers"""
+
+
+def add(a, b):
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Function returns number to the power of the other"""
+
+
+def pow(a, b):
+    return (a ** b)
