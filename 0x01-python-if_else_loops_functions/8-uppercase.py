8bb9c5801786ce531bfdd9591180361f1b518236
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-08-08 13:28:47 +0200

    Convert String to Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function convert string to uppercase only"""
+
+
+def uppercase(str):
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            ch = chr(ord(ch) - 32)
+        print("{}".format(ch), end="")
+    print("")
