58b70796ca5d3a40fe5f6fb33fc9748963c70058
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-08-10 08:05:16 +0200

    System Args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
