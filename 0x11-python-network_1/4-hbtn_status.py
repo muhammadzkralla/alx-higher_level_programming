9f9a50715be1fc089c7c1b8704f11c91f2a43230
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-12-01 23:36:56 +0200

    Status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,9 +3,12 @@
 """
 import requests
 
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)  # Send a GET request to the URL
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    content = response.text  # Get the content of the response
+
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
