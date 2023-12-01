b5e87e42d8336955f71c57b4989be7540a8c3756
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-12-01 23:40:36 +0200

    pycode ?

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,12 +4,17 @@
 - makes a request to the URL,
 - show the value of the X-Request-Id variable.
 """
+import requests
 import sys
-import urllib.request
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+if __name__ == '__main__':
+    url = sys.argv[1]  # Get the URL from command line argument
+    email = sys.argv[2]  # Get the email from command line argument
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    payload = {'email': email}  # Create the payload with the email
+
+    # Send a POST request with the payload
+    response = requests.post(url, data=payload)
+
+    print("Your email is: {}".format(email))
+    print(response.text)  # Print the content of the response
