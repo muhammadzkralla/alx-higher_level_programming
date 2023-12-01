7f728eb21dcdff368ef523911be974715ddbad51
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-12-01 22:57:16 +0200

    Using with

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,9 @@
 
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-response = urllib.request.urlopen(url)
-content = response.read().decode('utf-8')
-
-print("Body response:")
-print(f"\t- type: {type(content)}")
-print(f"\t- content: {content}")
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
