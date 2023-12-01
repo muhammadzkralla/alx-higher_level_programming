d540acae273896d74787ed80572117dd495e0c7c
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-12-01 23:21:45 +0200

    pycode

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,9 +14,11 @@ if __name__ == '__main__':
     url = sys.argv[1]  # Get the URL from command line argument
     email = sys.argv[2]  # Get the email from command line argument
 
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')  # Encode the email as URL parameter
+    data = urllib.parse.urlencode({'email': email}).encode(
+        'utf-8')  # Encode the email as URL parameter
 
     with urllib.request.urlopen(url, data) as response:
-        body = response.read().decode('utf-8')  # Read and decode the body of the response
+        # Read and decode the body of the response
+        body = response.read().decode('utf-8')
 
     print(body)
