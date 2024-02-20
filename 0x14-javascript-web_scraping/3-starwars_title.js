217082048e722ee2b432e3ba1e497d4b6b8fa192
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-20 20:05:35 +0200

    format

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,12 +6,12 @@ const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else if (response.statusCode === 200) {
-        const responseJSON = JSON.parse(body);
-        console.log(responseJSON.title);
-    } else {
-        console.log('Error code: ' + response.statusCode);
-    }
+	if (error) {
+		console.log(error);
+	} else if (response.statusCode === 200) {
+		const responseJSON = JSON.parse(body);
+		console.log(responseJSON.title);
+	} else {
+		console.log('Error code: ' + response.statusCode);
+	}
 });
