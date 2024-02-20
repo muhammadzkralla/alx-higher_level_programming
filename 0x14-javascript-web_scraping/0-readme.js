6894e06da4e6e46e6732e602f0284bc4dfc0c487
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-20 19:42:28 +0200

    Fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,18 +3,14 @@
 const fs = require('fs');
 
 function readFile(filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(data);
-        }
-    });
+	fs.readFile(filePath, 'utf-8', (err, data) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log(data);
+		}
+	});
 }
 
-if (process.argv.length !== 3) {
-    console.log("Usage: node 0-readme.js <file_path>");
-} else {
-    const filePath = process.argv[2];
-    readFile(filePath);
-}
+const filePath = process.argv[2];
+readFile(filePath);
