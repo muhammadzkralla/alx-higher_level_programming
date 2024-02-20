1dd548dec7311af3217abf28bdbe34e974a5b80a
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-20 19:46:30 +0200

    Fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,6 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(data);
-	}
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
