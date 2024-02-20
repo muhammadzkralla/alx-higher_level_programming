c4da63d4b0d97b912c874c83bd4ebce5eab3f564
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-20 20:06:40 +0200

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,13 +5,13 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(apiUrl, (error, response, body) => {
-	if (error) {
-		console.log(error);
-	} else if (response.statusCode === 200) {
-		const responseJSON = JSON.parse(body);
-		console.log(responseJSON.title);
-	} else {
-		console.log('Error code: ' + response.statusCode);
-	}
+request(apiUrl, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
 });
