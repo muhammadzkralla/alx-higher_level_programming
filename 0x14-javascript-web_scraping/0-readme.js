5b21705cc201f46043b4dd6fcbb61138cad22da0
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-20 19:45:23 +0200

    fun removed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,15 +2,10 @@
 
 const fs = require('fs');
 
-function readFile(filePath) {
-	fs.readFile(filePath, 'utf-8', (err, data) => {
-		if (err) {
-			console.error(err);
-		} else {
-			console.log(data);
-		}
-	});
-}
-
-const filePath = process.argv[2];
-readFile(filePath);
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+	if (err) {
+		console.error(err);
+	} else {
+		console.log(data);
+	}
+});
