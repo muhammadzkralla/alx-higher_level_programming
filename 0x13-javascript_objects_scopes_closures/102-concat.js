773141101adbf545113a8fb19b7f43a3e60e0141
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-27 23:13:23 +0200

    concat

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const f1 = fs.readFileSync(process.argv[2]).toString();
+const f2 = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], f1 + f2);
