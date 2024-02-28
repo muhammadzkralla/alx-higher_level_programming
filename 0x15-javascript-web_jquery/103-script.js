61a7e7cfc21aeeaa1c13f55f746f48cdd4575089
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-28 20:11:17 +0200

    translate

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    function fetchTranslation() {
+        const languageCode = $('#language_code').val();
+        $.getJSON(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function(data) {
+            $('#hello').text(data.hello);
+        });
+    }
+
+    $('#btn_translate').click(fetchTranslation);
+
+    $('#language_code').keypress(function(event) {
+        if (event.which == 13) {
+            fetchTranslation();
+        }
+    });
+});
